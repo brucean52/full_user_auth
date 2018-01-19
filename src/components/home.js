793ecf455c5d08597a25df62235a5e5b8a0957e6
brucean52 <bruce.an52@gmail.com>
2018-01-19 15:27:19 -0800

    routing complete

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+    return(
+        <div>
+            <h1 className="center-align">Welcome to Movie Quotes</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, quis?</p>
+        </div>
+    )
+}

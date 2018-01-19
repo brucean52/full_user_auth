8f54370bc20aa9bc6881dd326a74a2c9fdb91bc8
brucean52 <bruce.an52@gmail.com>
2018-01-19 15:29:22 -0800

    branch switch

--- src/components/movie_quotes.js
+++ src/components/movie_quotes.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieQuotes = props => {
     return (
         <div>
-            <h1 className="center-align">Movie Quote</h1>
+            <h1 className="center-align">Movie Quotes</h1>
             <p>Youtube Money</p>
         </div>
     );

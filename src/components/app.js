e0ad31cffd63e37b8e85347ec80ded9b0c179b89
brucean52 <bruce.an52@gmail.com>
2018-01-19 14:54:41 -0800

    navbar complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import Nav from './nav';
 
 const App = () => (
     <div>
+        <Nav/>
         <h1 className="center-align">User Auth</h1>
     </div>
 );
